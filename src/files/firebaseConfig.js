ca0bdfafd110faee56d29d9d0a1736bc07775e52
raburu <raburuochieng@gmail.com>
2024-03-27 06:08:03 +0300

    changes made to firebase credentials

---
+++ src/files/firebaseConfig.js
@@ -0,0 +1,13 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxLyFI4_FuhLhVAzcA4Z97Juu6wvPjQ_k",
+  authDomain: "authentication-app-f7486.firebaseapp.com",
+  projectId: "authentication-app-f7486",
+  storageBucket: "authentication-app-f7486.appspot.com",
+  messagingSenderId: "664282712734",
+  appId: "1:664282712734:web:b5ae32bcbbb2dfc9a4c98f"
+};
+
+export default firebaseConfig;
--- src/files/login.js
+++ src/files/login.js
@@ -8,31 +8,12 @@ import useAuthStore from './authStore';
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import firebaseConfig from './firebaseConfig';
 
 
 function Login() {
     const navigate = useNavigate();
 
-    // const successMessage = () => {
-    //     toast.success("Success Notification !", {
-    //       position: toast.POSITION.TOP_RIGHT,
-    //     });
-    //   };
-    const successMessage = () => {
-        toast.success("Success Notification !");
-    };
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDxLyFI4_FuhLhVAzcA4Z97Juu6wvPjQ_k",
-    authDomain: "authentication-app-f7486.firebaseapp.com",
-    projectId: "authentication-app-f7486",
-    storageBucket: "authentication-app-f7486.appspot.com",
-    messagingSenderId: "664282712734",
-    appId: "1:664282712734:web:b5ae32bcbbb2dfc9a4c98f"
-  };
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
